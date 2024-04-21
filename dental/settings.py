d3148f63518a38e9950a43b73b86fe495c7207b7
Henry Jonker <henryjonker67@hotmail.com>
2024-04-21 15:44:57 +0200

    Eamil aangepast

--- dental/settings.py
+++ dental/settings.py
@@ -148,7 +148,7 @@ EMAIL_USE_TLS = False
 '''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_PORT = 1025
+EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'henryjonker67@hotmail.com'
